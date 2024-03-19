7ad8276a32411fdf59bdbddff629050ab2a15aeb
KrytaniK <68296788+KrytaniK@users.noreply.github.com>
2024-03-18 20:11:58 -0500

    DisplayName Fix for Markdown Component

--- src/components/markdown/markdown.tsx
+++ src/components/markdown/markdown.tsx
@@ -4,12 +4,11 @@ import styles from "./markdown.module.css";
 import dynamic from "next/dynamic";
 import { TableOfContents } from "..";
 
-
 export default function Markdown({ sourceFile }: { sourceFile: string }) {
     const MDXContent = dynamic(() => import("/src/" + sourceFile).then((mod) => {
         return mod.default;
     }).catch(() => {
-        return () => <p style={{width: "100%", textAlign: "center"}}>Hmm... There's nothing here...</p>;
+        return () => <p style={{width: "100%", textAlign: "center"}}>Hmm... There&apos;s nothing here...</p>;
     }), {
         loading: () => <p>Loading Content...</p>,
     });
@@ -21,6 +20,9 @@ export default function Markdown({ sourceFile }: { sourceFile: string }) {
     }), {
         loading: () => <p>Loading Content...</p>,
     });
+
+    MDXContent.displayName = "MDXContent";
+    MDXHeadings.displayName = "MDXContent";
 
     return <section className={styles.markdownContainer}>
         {MDXHeadings && <MDXHeadings />}
